16b349f18b2f67f7afe2c734eecc050de080fb77
Marco Antônio Marchi Carreiro <antoniomarchi2006@gmail.com>
2023-09-10 17:17:28 -0300

    exercicio11 ok 100923

--- caldeiraAula1Java/src/caldeiraAula1Java/exercicio11.java
+++ caldeiraAula1Java/src/caldeiraAula1Java/exercicio11.java
@@ -6,12 +6,21 @@ public class exercicio11 {
     (a, e, i, o, u) estão presentes. Exiba o número de vogais na tela.
    */
 	public static void main(String[] args) {
+		//encontrei um problema após fazer o laço, ele só esta lendo vogais minusc. (a,e,i,o,u) e não
+		//(A,E,I,O,U).... Pode ser resolvido com este pequeno trecho
+		//de código  String palavraFrase = scanner.nextLine().toLowerCase();
+		//teste: AEIOUaeiou
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Digite uma palavra ou uma frase: ");
-		String palavraFrase = scanner.nextLine();
-		
-		
-		
+		String palavraFrase = scanner.nextLine().toLowerCase();
+		int vogais = 0;
+		   for (int i = 0; i < palavraFrase.length(); i++) {
+	            char caractere = palavraFrase.charAt(i);
+	            if (caractere == 'a' || caractere == 'e' || caractere == 'i' || caractere == 'o' || caractere == 'u') {
+	                vogais++;
+	            }
+	        }
+		    System.out.println("O número de vogais na palavra/frase é: " + vogais);
 		scanner.close();
 	}
 }
