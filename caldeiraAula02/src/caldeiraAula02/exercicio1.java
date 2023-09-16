d0ccfce6b382139b1abe2fb70e34fff5f34d122e
Marco Antônio Marchi Carreiro <antoniomarchi2006@gmail.com>
2023-09-15 23:13:57 -0300

    calculadora e números primos ok

--- caldeiraAula02/src/caldeiraAula02/exercicio1.java
+++ caldeiraAula02/src/caldeiraAula02/exercicio1.java
@@ -1,11 +1,39 @@
 package caldeiraAula02;
+import java.util.Scanner;
 
-/*Crie um programa que determine e
-escreva os números primos compreendidos entre um intervalo fornecido pelo usuário.
-*/
 public class exercicio1 {
 
-	public static void main(String[] args) {
-	System.out.println("oie");
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Determine números primos no intervalo que você quiser: ");
+        System.out.println("Digite o primeiro numero: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Digite o segundo numero: ");
+        int secondNumber = scanner.nextInt();
+
+        System.out.println("Intervalo escolhido [ " + firstNumber + " , " + secondNumber + " ]");
+        System.out.println("Numeros primos encontrados: ");
+
+        for (int i = firstNumber; i <= secondNumber; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+
+        scanner.close();
+    }
+
+    // Função para verificar se um número é primo
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
---
+++ caldeiraAula02/src/caldeiraAula02/exercicio2.java
@@ -0,0 +1,62 @@
+package caldeiraAula02;
+import java.util.Scanner;
+public class exercicio2 {
+
+public static void main(String[] args) {
+	   Scanner scanner = new Scanner(System.in);
+       boolean keepGoing = true;
+
+	  	System.out.println("Olá, seja bem-vindo a sua calculadora JAVA");
+	 while (keepGoing) {
+		 
+
+
+         System.out.println("Digite um primeiro número: ");
+         double firstNumber = scanner.nextDouble();
+         System.out.println("Digite um segundo número: ");
+         double secondNumber = scanner.nextDouble();
+
+         //Matar o bug da classe Scanner
+         scanner.nextLine();
+
+         System.out.println("Digite se quiser...");
+         System.out.println(" '+' Adicionar  ");
+         System.out.println(" '-' para Subtrair ");
+         System.out.println(" '*' para Multiplicar ");
+         System.out.println(" '/' para Dividir ");
+         System.out.println("ou digite N ou n para encerrar. ");
+         String option = scanner.nextLine().toLowerCase();
+         double result;
+         switch (option) {
+             case "+" -> {
+                 result = firstNumber + secondNumber;
+                 System.out.println(firstNumber + " + " + secondNumber + " = " + result);
+             }
+             case "-" -> {
+                 result = firstNumber - secondNumber;
+                 System.out.println(firstNumber + " - " + secondNumber + " = " + result);
+             }
+             case "*" -> {
+                 result = firstNumber * secondNumber;
+                 System.out.println(firstNumber + " * " + secondNumber + " = " + result);
+             }
+           
+             case "/" -> {
+                 if (secondNumber != 0.0) {
+                     result = firstNumber / secondNumber;
+                     System.out.println(firstNumber + " / " + secondNumber + " = " + result);
+                 } else {
+                     System.out.println("Erro: Divisão por zero.");
+                 }
+             }
+             case "n" -> {
+                 System.out.println("Adeus");
+                 keepGoing = false;
+             }
+             default -> System.out.println("Opção inválida!");
+         }
+      	System.out.println("Obrigadinho por calcular aqui, se quiser cancelar é só apertar 'N' ou 'n' ");
+     }
+     scanner.close();
+ }
+}
