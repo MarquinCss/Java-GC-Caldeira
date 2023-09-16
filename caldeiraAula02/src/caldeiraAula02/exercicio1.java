86729d23dade3f7d67092d6d6b6a0662ca8f9b76
Marco Antônio Marchi Carreiro <antoniomarchi2006@gmail.com>
2023-09-15 22:43:17 -0300

    att

---
+++ caldeiraAula02/src/caldeiraAula02/exercicio1.java
@@ -0,0 +1,11 @@
+package caldeiraAula02;
+
+/*Crie um programa que determine e
+escreva os números primos compreendidos entre um intervalo fornecido pelo usuário.
+*/
+public class exercicio1 {
+
+	public static void main(String[] args) {
+	System.out.println("oie");
+	}
+}
