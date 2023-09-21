53f1b18d1bfc4b6e700419cbbd49ae66b9fa8e8c
Marco Antônio Marchi Carreiro <antoniomarchi2006@gmail.com>
2023-09-20 23:14:46 -0300

    04

---
+++ aula04/src/geometrica/formaGeometrica.java
@@ -0,0 +1,15 @@
+package geometrica;
+
+public abstract class formaGeometrica {
+	  /* (construtor) oque vai ser o que 
+    public Veiculo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    */
+
+    // Método abstrato para calcular o custo da viagem com base na distância q o usuario der
+    public abstract double calcularArea(int area);
+
+}
